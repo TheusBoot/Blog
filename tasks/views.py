7b95c7883646a234890f0f13a038665ff3351408
Matheus <emailmartingcf1@gmail.com>
2020-05-07 22:28:52 -0300

    editando

--- tasks/views.py
+++ tasks/views.py
@@ -29,7 +29,18 @@ def newTask(request):
 	else:
 		form = TaskForm()
 		return render(request, 'tasks/addtask.html', {'form': form})
-	
+
+def editTask(request, id):
+	task = get_object_or_404(Task, pk=id)
+	form = TaskForm(instance=task)	
+
+	if (request.method == 'POST'):
+		return False
+
+	else:
+		return render(request, 'tasks/edittask.html',{'form':form, 'task': task})
+
+
 
 def yourname(request, name):
 	return render(request, 'tasks/yourname.html', {'name': name})
